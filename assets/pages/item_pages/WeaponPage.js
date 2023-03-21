1a56ee85523b822296ce6793bdc7fb9588013bd7
Vonkunken <brandonharry2015@gmail.com>
2023-03-21 23:47:57 +1100

    split name and damage to display as two seperate buttons, also added icons to list view, they are either swords or pistols for melee or ranged weapons

--- assets/pages/item_pages/WeaponPage.js
+++ assets/pages/item_pages/WeaponPage.js
@@ -1,6 +1,7 @@
 import { Dimensions, FlatList, StyleSheet, Text, View } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import weapons from '../../json/items_json/weapons.json'
+import weapons from '../../json/items_json/weapons.json';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function WeaponPage({ navigation }) {
     console.log(weapons);
@@ -9,13 +10,28 @@ export default function WeaponPage({ navigation }) {
             <FlatList 
                 data={weapons.weapon}
                 renderItem={({item}) =>
-                <TouchableOpacity
+                <View style={{ flexDirection:"row"}}>
+                    {iconChecker(item.type)}
+                    <TouchableOpacity
+                    style={styles.item}
+                    // onPress={() => onPressHandlerItem}
+                    >
+                    <Text style={styles.text}>{item.name}</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                    style={styles.item2}
+                    // onPress={() => onPressHandlerItem}
+                    >
+                    <Text style={styles.text}>{item.damage}</Text>
+                    </TouchableOpacity>
+                </View>}
+                /* <TouchableOpacity>
                     style={styles.item}
                     // onPress={() => onPressHandlerItem}
                     >
                     <Text style={styles.text}>{item.name}</Text>
                     <Text style={styles.text}>{item.damage}</Text>
-                </TouchableOpacity>}
+                </TouchableOpacity> */
             />
         </View>
     );
@@ -27,6 +43,13 @@ export default function WeaponPage({ navigation }) {
 //     });
 //   }
 
+  function iconChecker(weaponType) {
+    if(weaponType == "Martial Blaster" || weaponType == "Simple Blaster") {
+        return <MaterialCommunityIcons name="pistol" size={36} color="black" style="item3"/>
+    } else {
+        return <MaterialCommunityIcons name="sword" size={36} color="black" style="item3"/>
+    }
+  }
   const styles = StyleSheet.create({
     container: {
       flex: 1,
@@ -35,17 +58,33 @@ export default function WeaponPage({ navigation }) {
       justifyContent: 'center',
     },
     item: {
-        padding: 10,
+        padding: 4,
         borderRadius: 4,
         borderWidth: 2,
         borderColor: "#941a1d",
         backgroundColor: '#595959',
-        width: Dimensions.get('window').width,
+        width: Dimensions.get('window').width * 0.5,
+      },
+      item2: {
+        padding: 4,
+        borderRadius: 4,
+        borderWidth: 2,
+        borderColor: "#941a1d",
+        backgroundColor: '#595959',
+        width: Dimensions.get('window').width * 0.4,
+      },
+      item3: {
+        padding: 4,
+        borderRadius: 4,
+        borderWidth: 2,
+        borderColor: "#941a1d",
+        backgroundColor: '#595959',
+        width: Dimensions.get('window').width * 0.1,
       },
     text: {
         color: '#ffffff',
         alignItems: 'center',
-        fontSize: 36,
-        height: 50,
+        fontSize: 24,
+        height: 32,
     },
   });
